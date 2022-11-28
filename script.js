064fae11ca4fe3f3682a94061e21054f7e41efc8
Alexey <abelenkov2006@gmail.com>
2022-11-28 23:21:55 +0300

    Its change

---
+++ script.js
@@ -0,0 +1,11 @@
+function scrollUp() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function scrollDown() {
+    document.body.scrollTop = 1990;
+    document.documentElement.scrollTop = 1990;
+}
+
+//Добавить дерганье .illustration при движении мыши
